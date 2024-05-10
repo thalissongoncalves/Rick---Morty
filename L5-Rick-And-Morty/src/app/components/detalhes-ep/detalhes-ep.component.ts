898938f43af0d6472effdea6c7993d69d3f4a1c5
thalissongoncalves <thalissongsilva70@gmail.com>
2024-05-10 17:03:55 -0300

    Finalizado a página de detalhes dos Episódios

--- L5-Rick-And-Morty/src/app/components/detalhes-ep/detalhes-ep.component.ts
+++ L5-Rick-And-Morty/src/app/components/detalhes-ep/detalhes-ep.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { CabecalhoComponent } from '../cabecalho/cabecalho.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { RickMortyService } from '../../rick-morty.service';
 
 @Component({
   selector: 'app-detalhes-ep',
   standalone: true,
-  imports: [],
+  imports: [CabecalhoComponent, SidebarComponent, CommonModule],
   templateUrl: './detalhes-ep.component.html',
   styleUrl: './detalhes-ep.component.scss'
 })
 export class DetalhesEpComponent {
+  id: number = 0;
+  episode: any[] = [];
+  charactersId: string[] = [];
+  characterNames: string[] = [];
+  characterImages: string[] = [];
 
+  constructor(private route: ActivatedRoute, private rickMortyFetchService: RickMortyService) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params["id"];
+    this.rickMortyFetchService.getEpisodeById(this.id)
+      .then((data: any) => {
+        this.episode = this.episode.concat(data);
+        this.charactersId = data.characters.map((ep: string) => {
+          const parts: string[] = ep.split('/');
+          return parts[parts.length - 1]
+        });
+        this.charactersId.map((ep) => {
+          let charId: number = Number(ep);
+          this.rickMortyFetchService.getCharacterById(charId)
+            .then((data: any) => {
+              this.characterNames = this.characterNames.concat(data.name);
+              this.characterImages = this.characterImages.concat(data.image);
+            })
+            .catch(error => {
+              console.error("Erro:", error);
+            }
+          )
+        })
+      })
+      .catch(error => {
+        console.error("Erro:", error);
+      }
+    );
+    
+  }
 }
