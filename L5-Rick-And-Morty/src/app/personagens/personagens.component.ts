6ba1d4726e902c3a85f33c53780afe96117372bf
thalissongoncalves <thalissongsilva70@gmail.com>
2024-05-10 00:38:43 -0300

    dando andamento na página de Personagens

--- L5-Rick-And-Morty/src/app/personagens/personagens.component.ts
+++ L5-Rick-And-Morty/src/app/personagens/personagens.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CabecalhoComponent } from '../cabecalho/cabecalho.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-personagens',
   standalone: true,
-  imports: [CabecalhoComponent],
+  imports: [CabecalhoComponent, SidebarComponent],
   templateUrl: './personagens.component.html',
   styleUrl: './personagens.component.scss'
 })
